981bf96873c71f483f035d84b828455857c89c69
Cleitonsub <cleitonrmacedo@gmail.com>
2021-08-26 10:03:31 -0300

    Exercicios 1 ao 4

--- fundamentos/bloco-4-introducao-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,35 @@
-const squirtle = "melhor pokemon inicial";
+const a = 10;
+const b = 15;
+const c = 20;
 
-console.log(squirtle);
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+
+if (a > b) {
+    console.log("A constante a é maior que b")
+} else {
+    console.log("A constante b é maior que a")
+}
+
+
+if (a > b) {
+    console.log("a é a maior constante")
+} else if (b > c) {
+    console.log("b é a maior constante")
+} else {
+    console.log("c é a maior constante")
+}
+
+
+if (a > 0) {
+    console.log("positive")
+} else if (a < 0) {
+    console.log("negative")
+} else {
+    console.log("zero")
+}
