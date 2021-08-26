4dce5649ce223e7aaad14a8b3948fe5f67b791d8
Cleitonsub <cleitonrmacedo@gmail.com>
2021-08-26 11:07:04 -0300

    Adicao do exercicio 7

--- fundamentos/bloco-4-introducao-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,9 @@
 const a = 10;
 const b = 15;
 const c = 20;
-const d = "bishop"
-let e = "BiShOp"
-
+const d = "bishop";
+let e = "BiShOp";
+let nota = 86;
 
 console.log(a + b)
 console.log(a - b)
@@ -53,3 +53,22 @@ if (d === e.toLowerCase()) {
 }
 
 
+if (nota >= 90) {
+    console.log("A")
+} else if (nota >= 80) {
+    console.log("B")
+} else if (nota >= 70) {
+    console.log("C")
+} else if (nota >= 60) {
+    console.log("D")
+} else if (nota >= 50) {
+    console.log("E")
+} else if (nota < 50) {
+    console.log("F")
+} else if (nota < 0) {
+    console.log("Erro: nota menor que 0%")
+} else if (nota > 100) {
+    console.log("Erro: nota menor que 100%")
+} else {
+    console.log("Erro")
+}
