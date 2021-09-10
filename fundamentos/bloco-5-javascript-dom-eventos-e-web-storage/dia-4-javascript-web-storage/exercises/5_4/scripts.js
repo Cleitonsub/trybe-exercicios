ebebffa77eafe34f8da9b0d97b30433ce5a19f71
Cleitonsub <cleitonrmacedo@gmail.com>
2021-09-10 11:23:37 -0300

    Adicao de mudanca de cores de background e texto dinamicos

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercises/5_4/scripts.js
@@ -0,0 +1,21 @@
+// Cor do fundo
+
+const inputBackgroundColor = document.querySelector("#input-background-color");
+const body = document.querySelector("body");
+
+function corFundo() {
+  body.style.backgroundColor = inputBackgroundColor.value;
+}
+
+inputBackgroundColor.addEventListener("keyup", corFundo);
+
+// Cor do texto
+
+const inputTextColor = document.querySelector("#input-text-color");
+const article = document.querySelector("article");
+
+function corTexto() {
+  article.style.color = inputTextColor.value;
+}
+
+inputTextColor.addEventListener("keyup", corTexto);
