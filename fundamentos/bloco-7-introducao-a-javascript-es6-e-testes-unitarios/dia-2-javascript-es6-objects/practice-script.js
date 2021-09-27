4b4b1e3d8fd33d7115348c9222a70a2435361dbd
Cleitonsub <cleitonrmacedo@gmail.com>
2021-09-27 10:29:29 -0300

    Setup do dia

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/practice-script.js
@@ -0,0 +1,34 @@
+// const customer = {
+//     firstName: 'Roberto',
+//     age: 22,
+//     job: 'Teacher',
+// };
+
+// function testeObject(objeto, chave, valor) {
+//     objeto[chave] = valor;
+// }
+
+
+// testeObject(customer, 'lastName', 'Ribeiro');
+// testeObject(customer, 'Nacionalidade', 'Brasileiro');
+// console.log(customer);
+
+//--------------------------------------------------------------
+
+// const student1 = {
+//     Html: 'Muito Bom',
+//     Css: 'Bom',
+//     JavaScript: 'Ótimo',
+//     SoftSkills: 'Ótimo',
+// };
+
+// function listSkills(student) {
+//     const arrayOfSkills = Object.keys(student);
+//     for (const index in arrayOfSkills) {
+//         console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//     }
+// }
+// listSkills(student1);
+
+//--------------------------------------------------------------
+
