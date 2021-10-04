50f003bd9f85148243032dbdd6d468fe6f8477b6
Cleitonsub <cleitonrmacedo@gmail.com>
2021-10-04 15:18:29 -0300

    Pratica de HOF

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,3 +1,29 @@
-const wakeUp = () => {
-    return 'Acordando!!';
-}
+// const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+  
+//   const isEven = (number) => {
+//     if (number % 2 === 0) {
+//       console.log(number, 'is even');
+//     }
+//   };
+  
+//   const isOdd = (number) => {
+//     if ((number % 2) > 0) {
+//       console.log(number, 'is odd');
+//     }
+//   };
+  
+//   repeat(3, isEven); // Testa quais números serão pares;
+//   repeat(3, isOdd); // Testa quais números serão ímpares;
+
+
+  const wakeUp = () => 'Acordando!!';
+  const goCofee = () => 'Bora tomar café!!';
+  const goSleep = () => 'Partiu dormir!!';
+
+  const doingThings = (func) => func();
+
+  console.log(doingThings(goCofee));
