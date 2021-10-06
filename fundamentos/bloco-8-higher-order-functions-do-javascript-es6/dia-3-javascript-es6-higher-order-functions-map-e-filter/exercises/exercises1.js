32297f66ce26b9c8e2a83a1b3bfa863772ee68b3
Cleitonsub <cleitonrmacedo@gmail.com>
2021-10-06 19:35:56 -0300

    Exercicio 1 do dia - map()

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercises/exercises1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercises/exercises1.js
@@ -63,4 +63,24 @@ const books = [
   
   // Adicione o código do exercício aqui:
 
+  // 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+  // Dica: Use a função map
+
+  const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
   
+  function formatedBookNames() {
+    // escreva seu código aqui
+    const convertBooks = books.map((book) => {
+        return `${book.name} - ${book.genre} - ${book.author.name}`
+    });
+    return convertBooks;
+  }
+
+  console.log(formatedBookNames());
