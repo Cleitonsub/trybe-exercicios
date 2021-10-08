c062c2a2e32c6a4f6a533479df81159fecf05ae7
Cleitonsub <cleitonrmacedo@gmail.com>
2021-10-08 16:22:23 -0300

    Praticas dos assuntos do dia

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testes do dia.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testes do dia.js
@@ -23,16 +23,58 @@ const [saudacao] = saudacoes;
 
 console.log(saudacao);
  //-----------------------------------------------------
+
 let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
 
 console.log(comida, animal, bebida); // arroz gato água
 
-// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// 2 - Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 
 const arrayDasLets = [comida, animal, bebida];
 
 const [animal1, bebida1, comida1] = arrayDasLets;
 
 console.log(animal1);
+//-----------------------------------------------------
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+
+const [] = numerosPares;
+
+console.log(numerosPares);
+//-----------------------------------------------------
+
+// DEFAULT DESTRUCTURING
+
+const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+const { nationality = 'Brazilian' } = person;
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+//-----------------------------------------------------
+
+// Object Property Shorthand
+
+const getPosition = (latitude, longitude) => ({
+    latitude,
+    longitude});
+  
+  console.log(getPosition(-19.8157, -43.9542));
