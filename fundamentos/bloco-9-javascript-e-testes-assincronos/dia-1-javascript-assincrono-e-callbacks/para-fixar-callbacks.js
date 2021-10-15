eae107503a6bb90156c13c5829ee3869fdc3e375
Cleiton <cleitonrmacedo@gmail.com>
2021-10-15 15:04:22 -0300

    Setup do dia

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/para-fixar-callbacks.js
@@ -0,0 +1,13 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = () => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+};
+
+console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
