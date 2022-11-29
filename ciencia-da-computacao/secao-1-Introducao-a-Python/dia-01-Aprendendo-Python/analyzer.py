e6945dac7637815711c25ab95e86f70e8e91cade
Cleiton <cleitonrmacedo@gmail.com>
2022-11-29 17:54:39 -0300

    Primeiras funcoes em python

---
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-01-Aprendendo-Python/analyzer.py
@@ -0,0 +1,19 @@
+def analyze(text):
+  # ... = cria função sem desenvolver
+  # 
+  count_words = len(text.split(" "))
+  count_letters = len(text)
+
+  # contagem de ocorrências por caracter
+  char_counter = dict()
+  for char  in text:
+    if char in char_counter:
+      char_counter[char] += 1
+    else:
+      char_counter[char] = 1
+
+  return f"Esse texto possui {count_words} palavras e {count_letters} letras no total.\nOcorrência por caracter = {char_counter}"
+
+text_to_analyze = ("Hehe, texto gigante")
+
+print(analyze(text_to_analyze))
---
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-01-Aprendendo-Python/samply1.py
@@ -0,0 +1,7 @@
+age = 18
+
+print(age)
+
+names = ['Cleiton', 'Isamim', 'Walter', 'Guilherme']
+
+print(names)
