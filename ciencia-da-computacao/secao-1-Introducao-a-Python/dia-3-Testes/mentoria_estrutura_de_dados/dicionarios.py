d5925d3aee12a2b23d61a8376362e1757291c13a
Cleiton <cleitonrmacedo@gmail.com>
2022-12-01 19:08:46 -0300

    feat: como adiciona e acessa valores de um dicionario

--- ciencia-da-computacao/secao-1-Introducao-a-Python/dia-3-Testes/mentoria_estrutura_de_dados/dicionarios.py
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-3-Testes/mentoria_estrutura_de_dados/dicionarios.py
@@ -16,3 +16,17 @@ print(f"Simplificado {cities_dict}")
 cities_dict = dict(cities_list)
 
 print(f"Mais simplificado ainda {cities_dict}")
+
+#---------------------------------
+cities_dict['Guilherme'] = 'Contagem' # adiciona no dicionário
+
+print(f"Outra forma de criar um dicionário {cities_dict}")
+
+cities_dict["Guilherme"] = 'Algum lugar' # com a mesma chave ele sobrescreve
+
+print(f"Outra forma de criar um dicionário {cities_dict}")
+
+#---------------------------------
+print(f"Valor comum {cities_dict['Guilherme']}")
+
+print(f"Como chamar dois valores {cities_dict['Guilherme']}, {cities_dict['Felipe']}")
