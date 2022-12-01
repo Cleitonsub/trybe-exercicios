415597bf81725ce03042748becf4999e3573c894
Cleiton <cleitonrmacedo@gmail.com>
2022-12-01 19:48:03 -0300

    feat: Manipulando conjuntos

--- ciencia-da-computacao/secao-1-Introducao-a-Python/dia-3-Testes/mentoria_estrutura_de_dados/dicionarios.py
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-3-Testes/mentoria_estrutura_de_dados/dicionarios.py
@@ -30,3 +30,22 @@ print(f"Outra forma de criar um dicionário {cities_dict}")
 print(f"Valor comum {cities_dict['Guilherme']}")
 
 print(f"Como chamar dois valores {cities_dict['Guilherme']}, {cities_dict['Felipe']}")
+
+print(f"Como pesquisar alguém inexistente sem o código quebrar {cities_dict.get('Thiago')}") # return none
+
+if 'Santo André' in cities_dict.values(): # pesquisa pelo valor
+    print('Vou visitar a Elaine')
+
+print(cities_dict.keys()) # return dict_keys(['Elaine', 'Felipe', 'Guilherme'])
+
+conjunto = set() # é igual ao dicionário mas apenas os valores (não aceita valores repetidos)
+conjunto2 = set({'Rio de Janeiro', 'Recife'}) # cria conjunto2
+
+conjunto.add('Santo André')
+conjunto.add('Rio de Janeiro')
+conjunto.add('Belo Horizonte')
+
+print(conjunto)
+print(conjunto2)
+
+print(conjunto.difference(conjunto2)) # diferença do conjunto para o conjunto2 (pega tudo do 1)
